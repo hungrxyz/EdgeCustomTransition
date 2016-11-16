4063166c2f07933de189345e9d7e8f100ee9fb87
zzzel <marko@zzzel.xyz>
2016-11-16 02:30:01 +0100

    Initial commit

--- EdgeCustomTransition/ViewController.swift
+++ EdgeCustomTransition/ViewController.swift
@@ -15,6 +15,22 @@ class ViewController: UIViewController {
 		// Do any additional setup after loading the view, typically from a nib.
 	}
 
+	@IBAction func leftTapped(_ sender: Any) {
+		let vc = ViewController(nibName: String(describing: ViewController.self), bundle: nil)
+		vc.view.backgroundColor = .yellow
+		self.present(vc, animated: true, completion: nil)
+	}
+	
+	@IBAction func rightTapped(_ sender: Any) {
+		let vc = ViewController(nibName: String(describing: ViewController.self), bundle: nil)
+		vc.view.backgroundColor = .green
+		self.present(vc, animated: true, completion: nil)
+	}
+	
+	@IBAction func dismissTapped(_ sender: Any) {
+		self.dismiss(animated: true, completion: nil)
+	}
+	
 	override func didReceiveMemoryWarning() {
 		super.didReceiveMemoryWarning()
 		// Dispose of any resources that can be recreated.
